bd5dd57e117a0784510cb9f74990ce41a4ba5337
NineThou <shelkov7000@gmail.com>
2018-02-22 19:17:44 +0200

    Add fish list

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+	goToStore(e) {
+		e.preventDefault();
+		//grab the text from the box
+		//this.storeInput.value;
+		//transition to /store/:storeId
+		this.context.router.transitionTo(`/store/${this.storeInput.value}`)
+	}
+
 	render() {
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={this.goToStore.bind(this)}>
 				<h2>Please Enter a Store</h2>
-				<input type="text" required placeholder="Store Name"/>
+				<input type="text" required placeholder="Store Name" default value={getFunName()} ref={input => this.storeInput = input}/>
 				<button type="submit">Visit Store</button>
 			</form>
 		)
 	}
+}
+
+StorePicker.contextTypes = {
+	router: React.PropTypes.object
 }
 
 export default StorePicker;
