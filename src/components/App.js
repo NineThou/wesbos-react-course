cb7b357c0bec7c02b21f164f61cfe5360e2683dd
NineThou <shelkov7000@gmail.com>
2018-02-28 02:27:47 +0200

    Add remove from inventory and store feature

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,9 @@ class App extends React.Component {
 		this.addFish = this.addFish.bind(this);
 		this.loadSamples = this.loadSamples.bind(this);
 		this.addToOrder = this.addToOrder.bind(this);
+		this.updateFish = this.updateFish.bind(this);
+		this.removeFish = this.removeFish.bind(this);
+		this.removeFromOrder = this.removeFromOrder.bind(this);
 
 		//getInitialState
 		this.state = {
@@ -23,12 +26,17 @@ class App extends React.Component {
 
 	//firebase
 	componentWillMount() {
+		//firebase
 		this.ref = base.syncState(`${this.props.params.storeId}/fishes`, {
 			context: this,
 			state: 'fishes'
 		});
+		//localStorage
+		localStorage.getItem(`order-${this.props.params.storeId}`) && this.setState({
+			order: JSON.parse(localStorage.getItem(`order-${this.props.params.storeId}`)),
+		})
 	}
-
+	//remove binding with firebase
 	componentWillUnmount() {
 		base.removeBinding(this.ref);
 	}
@@ -42,6 +50,32 @@ class App extends React.Component {
 		fishes[`fish-${timestamp}`] = fish;
 		//set state
 		this.setState({ fishes });
+	}
+
+	updateFish(key, updatedFish) {
+		const fishes = {...this.state.fishes};
+		fishes[key] = updatedFish;
+		this.setState({ fishes });
+	}
+
+	removeFish(key) {
+		const fishes = {...this.state.fishes}
+		fishes[key] = null;
+		this.setState({
+			fishes
+		});
+	}
+
+	removeFromOrder(key) {
+		const order = {...this.state.order}
+		delete order[key];
+		this.setState({
+			order
+		});
+	}
+
+	componentWillUpdate(nextProps, nextState) {
+		localStorage.setItem(`order-${this.props.params.storeId}`, JSON.stringify(nextState.order));
 	}
 	
 	loadSamples() {
@@ -73,8 +107,19 @@ class App extends React.Component {
 						}
 					</ul>
 				</div>
-				<Order fishes={this.state.fishes} order={this.state.order}/>
-				<Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
+				<Order 
+					fishes={this.state.fishes} 
+					order={this.state.order}
+					params={this.props.params}
+					removeFromOrder={this.removeFromOrder}
+				/>
+				<Inventory 
+					addFish={this.addFish} 
+					loadSamples={this.loadSamples}
+					fishes={this.state.fishes}
+					updateFish={this.updateFish}
+					removeFish={this.removeFish}
+				/>
 			</div>
 		)
 	}
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -2,10 +2,37 @@ import React from 'react';
 import AddFishForm from './AddFishForm'
 
 class Inventory extends React.Component {
+	handleChange(e, key) {
+		const fish = this.props.fishes[key];
+		const updatedFish = {
+			...fish,
+			[e.target.name] : e.target.value 
+		}
+		this.props.updateFish(key, updatedFish);	
+	}
+
+	renderInventory(key) {
+		const fish = this.props.fishes[key];
+		return(
+			<div className="fish-edit" key={key}>
+				<input onChange={(e)=>this.handleChange(e, key)} name="name" value={fish.name} type="text" placeholder="Fish Name"/>
+				<input onChange={(e)=>this.handleChange(e, key)} name="price" value={fish.price} type="text" placeholder="Fish Price"/>
+				<select onChange={(e)=>this.handleChange(e, key)} name="status" value={fish.status} >
+					<option value="available">Fresh!</option>
+					<option value="unavailable">Sold Out!</option>
+				</select>
+				<textarea onChange={(e)=>this.handleChange(e, key)} name="desc" value={fish.desc}  placeholder="Fish Description"></textarea>
+				<input onChange={(e)=>this.handleChange(e, key)} name="image" value={fish.image} type="text" placeholder="Fish Image"/>
+				<button onClick={() => this.props.removeFish(key)}>Delete Fish</button>
+			</div>
+		)
+	}
+
 	render() {
 		return (
 			<div>
 				<h2>Inventory</h2>
+				{Object.keys(this.props.fishes).map(this.renderInventory.bind(this))}
 				<AddFishForm addFish={this.props.addFish}/>
 				<button onClick={this.props.loadSamples}>Load Sample Fishes</button>
 			</div>
